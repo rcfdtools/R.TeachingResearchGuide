e0c5f5c3b85f8ae95832a827bcbc28c774e6348c
r.cfdtools <r.cfdtools@gmail.com>
2022-09-09 07:41:17 -0500

    General update.

--- .src/test.py
+++ .src/test.py
@@ -16,10 +16,10 @@ projectile_txt = 'Projectile refers to an object that is in flight after being t
 file_header = ('### Parabolic trajectory projectile motion - Markdown example\n\n'
                '%s\n\n'
                '$y = x\\tan \\theta  - \\frac{{g{{(x)}^2}}}{{2{{(u\cos \\theta )}^2}}}\$'
-               '\n\nDonde\n\n'
-               '$g$ = gravity acceleration, m/s²: %f\n\n'
-               '$\\theta$ = inclination angle, °: %f\n\n'
-               '$u$ = initial projectile velocity, m/s: %f'
+               '\n\nDonde,\n\n'
+               '$g$ = gravity acceleration, m/s²: %f'
+               '\n$\\theta$ = inclination angle, °: %f'
+               '\n$u$ = initial projectile velocity, m/s: %f'
                '\n\n#### Results table' %(projectile_txt, g,theta,u)
               )
 print(file_header)
